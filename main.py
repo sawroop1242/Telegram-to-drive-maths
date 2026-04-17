edc735b1d7f21569eb67cd0d04114c93e2bba8da
sawroop1242 <swroopss487@gmail.com>
2026-04-17 09:21:05 +0530

    Update channel ID and save path in main.py

--- main.py
+++ main.py
@@ -9,10 +9,10 @@ api_id = int(os.environ['API_ID'])
 api_hash = os.environ['API_HASH']
 session_string = os.environ['TELEGRAM_SESSION']
 
-channel_id = -3780239942
+channel_id = -1003711164317
 
 # The path where GitHub Actions will mount your Google Drive
-save_path = '/content/drive/MyDrive/Telegram_Archive/Maths/live_betch/'
+save_path = '/content/drive/MyDrive/Telegram_Archive/Maths/spartan/'
 os.makedirs(save_path, exist_ok=True)
 
 async def main():
